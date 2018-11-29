d558601d60e795150efe0bca0a8739a52dcc7827
Myles Andre <mylesandre1124@gmail.com>
2018-11-29 17:30:59 -0500

    Formatted some strings

--- src/decorator/WelcomeMessageDecorator.java
+++ src/decorator/WelcomeMessageDecorator.java
@@ -11,10 +11,10 @@ public class WelcomeMessageDecorator extends ItineraryDecorator {
         System.out.println("Itinerary by " + decoratedItinerary.getTrip().getReserver().getName() + ", "
                 + decoratedItinerary.getTrip().getReserver().getPhoneNumber() +"\n" +
                 decoratedItinerary.getTrip().getMainTraveller().getName() + " – Your trip to Fiji is all set. " +
-                "I have booked top-quality transport for every leg of your trip. " +
-                "I am including a fine bottle of your favorite drink, Boones Farm Sunshine Pink, " +
-                "to enjoy along the way. If you have any questions or problems, call me anytime. " +
-                "Safe travels! " + decoratedItinerary.getTrip().getReserver().getName() + "Certified Premium Travel Service Agent\n");
+                "\nI have booked top-quality transport for every leg of your trip. " +
+                "\nI am including a fine bottle of your favorite drink, Boones Farm Sunshine Pink, " +
+                "to enjoy along the way. \nIf you have any questions or problems, call me anytime. " +
+                "Safe travels! " + decoratedItinerary.getTrip().getReserver().getName() + " Certified Premium Travel Service Agent\n");
     }
 
 
